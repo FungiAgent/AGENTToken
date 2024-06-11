97eea77f98b95f781e282ecf81c1397ce60aae58
dreski <dreskirim@gmail.com>
2024-06-11 12:16:15 +0200

    ..

--- script/DeployAGENT.s.sol
+++ script/DeployAGENT.s.sol
@@ -1,12 +1,25 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.19;
+pragma solidity ^0.8.20;
 
 import "forge-std/Script.sol";
+import "src/AGENTToken.sol";
 
 contract DeployAGENT is Script {
-    function setUp() public {}
+    function run() external returns (AGENTToken) {
+        vm.startBroadcast();
 
-    function run() public {
-        vm.broadcast();
+        /* 
+            * Deploy AGENTToken contract
+            * 
+            * Parameters:
+            * - owner: address of the contract owner
+            * - maxSupply: maximum supply of the token
+            * - uri: token URI (logo image)
+            * - taxPercentage: tax percentage in basis points
+            * - taxRecipient: address of the tax recipient
+        */
+        AGENTToken agentToken = new AGENTToken(address(this), 999999999, "https://token-uri.com", 100, address(0x4));
+        vm.stopBroadcast();
+        return agentToken;
     }
 }
